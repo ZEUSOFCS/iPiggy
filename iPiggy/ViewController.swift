2f6334e2af338593e29c6b9de56931f82cd7d255
DIEGO BROWN <ZEUSOFOHIO@diegos-macbook.local>
2017-05-18 01:17:45 -0400

    [CODE IMPROVEMENT]

--- iPiggy/ViewController.swift
+++ iPiggy/ViewController.swift
@@ -13,8 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Hide label (make invisable)
+        devLabel.isHidden = true
+        nameLabel.isHidden = true
+        statusLabel.isHidden = true
     }
 
+    // Labels
+    @IBOutlet weak var devLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+
+    
+    // Buttons
+    @IBAction func touchButtonDidTouch(_ sender: Any) {
+        
+        // unHide label (make visable)
+        devLabel.isHidden = false
+        nameLabel.isHidden = false
+        statusLabel.isHidden = false
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
